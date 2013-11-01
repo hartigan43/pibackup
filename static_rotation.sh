233251b4c89a952e6304fe15b88624f40fe517d1
Jake Hartigan <jake@omni-dev.com>
2013-11-01 16:22:12 -0400

    Added dbs to monthly archival

--- static_rotation.sh
+++ static_rotation.sh
@@ -2,6 +2,9 @@
 #This script is run by cron monthly and compresses all of the static files used in clients websites
 date=$(date '+%Y_%m_%d')
 
-tar -czf /home/backups/backups/archives/"$date".tar.gz /home/backups/files/* --verify
+#compress current rsync directory and save it
+tar -czf /home/backups/backups/archives/files_"$date".tar.gz /home/backups/files/* --verify
 
-
+#copy the most recent db archive, a week, and a month into the archives
+tocopy=$(ls /home/backups/backups/databases | tail -1 )
+cp /home/backups/backups/databases/"$tocopy" /home/backups/backups/archives
