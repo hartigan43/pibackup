e6ed5bb0684a95eb11fbcce25b2c0f9a36613bce
Jake Hartigan <jake@omni-dev.com>
2013-11-06 17:27:46 -0500

    Syntax corrections and some added archival settings

--- static_rotation.sh
+++ static_rotation.sh
@@ -1,5 +1,5 @@
 # /bin/bsh
-#This script is run by cron monthly and compresses all of the static files used in clients websites
+#This script is run by cron weekly and compresses all of the static files used in clients websites
 date=$(date '+%Y_%m_%d')
 
 #compress current rsync directory and save it
@@ -8,3 +8,10 @@ tar -czf /home/backups/backups/archives/files_"$date".tar.gz /home/backups/files
 #copy the most recent db archive, a week, and a month into the archives
 tocopy=$(ls /home/backups/backups/databases | tail -1 )
 cp /home/backups/backups/databases/"$tocopy" /home/backups/backups/archives
+tocopy=$(find /home/backups/backups/databases -type f -mtime +6)
+cp /home/backups/Backups/databases/"$tocopy" /home/backups/Backups/archives
+tocopy=$(find /home/backups/backups/databases -type f -mtime +29)
+cp /home/backups/Backups/databases/"$tocopy" /home/backups/Backups/archives
+
+#remove files older than 45 days in the archives directory
+find /home/backups/Backups/archives/*.tar.gz -mtime +44 -exec rm {} \;
